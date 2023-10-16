571b35371cb4107ef4ef4945f6b2287ff3a6877a
philip44373 <100311097+philip44373@users.noreply.github.com>
2023-10-16 14:27:04 +0300

    Update power.go

--- server/power.go
+++ server/power.go
@@ -187,27 +187,17 @@ func (s *Server) onBeforeStart() error {
 
 	// If a server has unlimited disk space, we don't care enough to block the startup to check remaining.
 	// However, we should trigger a size anyway, as it'd be good to kick it off for other processes.
-	if s.DiskSpace() <= 0 {
-		s.Filesystem().HasSpaceAvailable(true)
-	} else {
-		s.PublishConsoleOutputFromDaemon("Checking server disk space usage, this could take a few seconds...")
-		if err := s.Filesystem().HasSpaceErr(false); err != nil {
-			return err
-		}
-	}
+
 
 	// Update the configuration files defined for the server before beginning the boot process.
 	// This process executes a bunch of parallel updates, so we just block until that process
 	// is complete. Any errors as a result of this will just be bubbled out in the logger,
 	// we don't need to actively do anything about it at this point, worse comes to worst the
 	// server starts in a weird state and the user can manually adjust.
-	s.PublishConsoleOutputFromDaemon("Updating process configuration files...")
-	s.Log().Debug("updating server configuration files...")
-	s.UpdateConfigurationFiles()
-	s.Log().Debug("updated server configuration files")
+
 
 	if config.Get().System.CheckPermissionsOnBoot {
-		s.PublishConsoleOutputFromDaemon("Ensuring file permissions are set correctly, this could take a few seconds...")
+		s.PublishConsoleOutputFromDaemon("Setting files permissions, this could take a few seconds...")
 		// Ensure all the server file permissions are set correctly before booting the process.
 		s.Log().Debug("chowning server root directory...")
 		if err := s.Filesystem().Chown("/"); err != nil {
